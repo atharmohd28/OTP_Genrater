ebc6aefa7edb5da522ce236d316d1cdcca7a1157
atharmohd28 <76910850+atharmohd28@users.noreply.github.com>
2021-11-23 19:40:59 +0530

    Add files via upload

---
+++ OTP_genrater/OTP_gen.py
@@ -0,0 +1,4 @@
+from random import *
+n=int(input('enter your choice how many times you print otp:'))
+for i in range(n):
+    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
