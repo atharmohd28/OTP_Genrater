6e978d80f61b9085d07d1a32bb3c56bb3ad4ac63
atharmohd28 <76910850+atharmohd28@users.noreply.github.com>
2021-11-23 19:44:31 +0530

    Delete OTP_gen.py

--- OTP_genrater/OTP_gen.py
+++
@@ -1,4 +0,0 @@
-from random import *
-n=int(input('enter your choice how many times you print otp:'))
-for i in range(n):
-    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
